c6430cdfa11f2f151034a8616f164e4707338ded
Esteban <davidvyce@hotmail.com>
2025-09-09 12:14:46 -0500

    haha

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CharReveal from "./components/charReveal";
 import TextReveal from "./components/textReveal";
 import AnimatedText from "./components/animate-text";
 
 const App = () => {
+  useEffect(() => {
+    document.body.style.opacity = 1;
+  }, []);
+
   return (
     <>
       <div className=" max-w-[1080px] mx-auto p-3 ">
